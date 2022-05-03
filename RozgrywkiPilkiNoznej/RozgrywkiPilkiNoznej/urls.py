cae69df1397378417a3ee1816112a8c80a201060
Pawel12JAVA <pawelorzel1999@gmail.com>
2022-05-03 13:05:05 +0200

    login for organizator

--- RozgrywkiPilkiNoznej/RozgrywkiPilkiNoznej/urls.py
+++ RozgrywkiPilkiNoznej/RozgrywkiPilkiNoznej/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('football_league/', include('football_league.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
