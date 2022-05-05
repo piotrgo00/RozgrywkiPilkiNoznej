f45da9019abd9efd1f63b4339a8debd514eaebf3
ErykGodles <eryk161@gmail.com>
2022-05-05 02:08:24 +0200

    player_details; team_details;

--- RozgrywkiPilkiNoznej/RozgrywkiPilkiNoznej/urls.py
+++ RozgrywkiPilkiNoznej/RozgrywkiPilkiNoznej/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('football_league/', include('football_league.urls')),
+    path('', include('football_league.urls')),
     path('admin/', admin.site.urls),
 ]
--- RozgrywkiPilkiNoznej/football_league/urls.py
+++ RozgrywkiPilkiNoznej/football_league/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'football_league'
 urlpatterns = [
     # ex: /football_league/
+    path('', views.home, name='home'),
     path('match/', views.MatchIndexView.as_view(), name='match_index'),
     path('team/', views.TeamIndexView.as_view(), name='team_index'),
     path('team/<int:pk>/', views.DetailTeamView.as_view(), name='team_detail'),
+    path('player/<int:pk>/', views.DetailPlayerView.as_view(), name='player_detail'),
 ]
